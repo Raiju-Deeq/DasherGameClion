873cfb62b2f7435bc9790666a72c4d78853c5c71
Raiju-Deeq <mdeeq0@me.com>
2025-04-13 20:06:53 +0100

    Learnt about struct type and embedded data type. Used struct for AnimData for Scarfy and Nebula

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "raylib.h"
 
+struct AnimData
+{
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updateTime;
+    float runningTime;
+};
+
 int main()
 {
+    // Initialize the window
     const int windowWidth {800}; // Set the width of the window
     const int windowHeight {600}; // Set the height of the window
     InitWindow (windowWidth,windowHeight, "Dasher Game");
+    SetTargetFPS (60);
+
 
     int velocity{0}; // Set the initial velocity of the rectangle
     bool isAirborne{false}; // Set the initial state of jumping to false
@@ -12,42 +24,43 @@ int main()
     int jumpHeight{600}; // Set the jump height (In pixels per second)
 
 
+    // Scarfy variables
+    Texture2D scarfy = LoadTexture("textures/scarfy.png"); // Load the texture for the scarfy character
+    // Create a rectangle for the scarfy character
+    AnimData scarfyData; // Create a struct for the scarfy character
+    scarfyData.rec.width = scarfy.width/6; // Set the width of the rectangle based on the texture width
+    scarfyData.rec.height = scarfy.height; // Set the height of the rectangle based on the texture height
+    scarfyData.rec.x = 0; // Set the initial X position of the rectangle
+    scarfyData.rec.y = 0; // Set the initial Y position of the rectangle
+    scarfyData.pos.x = windowWidth/5 - scarfyData.rec.width/5; // Set the initial X position of the scarfy character
+    scarfyData.pos.y = windowHeight - scarfyData.rec.height; // Set the initial Y position of the scarfy character
+    scarfyData.frame = 0;
+    scarfyData.updateTime = 1.f/12.f; // Set the time between frames (12 frames per second)
+    scarfyData.runningTime = 0.0f; // Set the initial running time of the animation
+
     // Load the texture for nebula hazard
     Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png"); // Load the texture for the nebula hazard
-    Rectangle nebRec{0.0f, 0.0f, (float)nebula.width/8,(float)nebula.height/8}; // Create a rectangle for the nebula hazard
-    Vector2 nebPos{windowWidth, windowHeight - nebRec.height}; // Set the initial position of the nebula hazard
+
     int nebVelocity{-600}; // Set the initial velocity of the nebula hazard
-    // Nebula animation variables
-    int nebFrame{}; // Set the initial frame of the nebula animation
-    const float nebUpdateTime{1.f/12.f}; // Set the time between frames (12 frames per second)
-    float nebRunningTime{}; // Set the initial running time of the nebula animation
+    // AnimData struct for nebula
+    AnimData nebData{
+            {0.f, 0.f, (float)nebula.width/8, (float)nebula.height/8}, // Set the rectangle for the nebula hazard
+            {windowWidth, (float)windowWidth - nebula.height/8}, // Set the initial position of the nebula hazard
+            0, // Set the initial frame of the nebula animation
+            {1.f/12.f}, // Set the time between frames (12 frames per second)
+            0 // Set the initial running time of the nebula animation
+        };
+
+    // AnimData struct for nebula2
+    AnimData nebData2{
+                {0.f, 0.f, (float)nebula.width/8, (float)nebula.height/8}, // Set the rectangle for the nebula hazard
+                {windowWidth +300, (float)windowWidth - nebula.height/8}, // Set the initial position of the nebula hazard
+                0, // Set the initial frame of the nebula animation
+                {1.f/16.f}, // Set the time between frames (12 frames per second)
+                0 // Set the initial running time of the nebula animation
+            };
 
 
-
-
-
-
-    // Load the texture for the scarfy character
-    Texture2D scarfy = LoadTexture("textures/scarfy.png"); // Load the texture for the scarfy character
-    Rectangle scarfyRec; // Create a rectangle for the scarfy character
-    scarfyRec.width = scarfy.width/6;
-    scarfyRec.height = scarfy.height;
-    scarfyRec.x = 0; // Set the initial X position of the rectangle
-    scarfyRec.y = 0; // Set the initial Y position of the rectangle
-    Vector2 scarfyPos; // Create a vector for the position of the scarfy character
-    scarfyPos.x = windowWidth/5 - scarfyRec.width/5; // Set the initial X position of the scarfy character
-    scarfyPos.y = windowHeight - scarfyRec.height; // Set the initial Y position of the scarfy character
-
-
-    // Animation variables
-    int frame{}; // Set the initial frame of the animation
-    const float updateTime{1.f/12.f}; // Set the time between frames (12 frames per second)
-    float runningTime{}; // Set the initial running time of the animation
-
-
-
-
-    SetTargetFPS (60);
     while (!WindowShouldClose())
     {
         const float deltaTime = GetFrameTime(); // Get the time between frames
@@ -59,45 +72,62 @@ int main()
 
         //Update Scarfy animation
         if (!isAirborne) {
-            runningTime += deltaTime; // Update the running time
-            if (runningTime >= updateTime)
+            scarfyData.runningTime += deltaTime; // Update the running time
+            if (scarfyData.runningTime >= scarfyData.updateTime)
             {
-                runningTime = 0.0f; // Reset the running time
+                scarfyData.runningTime = 0.0f; // Reset the running time
                 // updated animation frame
-                scarfyRec.x = frame * scarfyRec.width; // Update the X position of the rectangle based on the frame
-                frame++; // Increment the frame
-                if (frame > 5) // Check if the frame is greater than 5
+                scarfyData.rec.x = scarfyData.frame * scarfyData.rec.width; // Update the X position of the rectangle based on the frame
+                scarfyData.frame++; // Increment the frame
+                if (scarfyData.frame > 5) // Check if the frame is greater than 5
                 {
-                    frame = 0; // Reset the frame to 0
+                    scarfyData.frame = 0; // Reset the frame to 0
                 }
             }
         }
 
         // Update nebula animation
-        nebRunningTime += deltaTime; // Update the running time
-        if (nebRunningTime >= nebUpdateTime)
+        nebData.runningTime += deltaTime; // Update the running time
+        if (nebData.runningTime>= nebData.updateTime)
         {
-            nebRunningTime = 0.0f; // Reset the running time
+            nebData.runningTime = 0.0f; // Reset the running time
             // updated animation frame
-            nebRec.x = nebFrame * nebRec.width; // Update the X position of the rectangle based on the frame
-            nebFrame++; // Increment the frame
-            if (nebFrame > 7) // Check if the frame is greater than 7
+            nebData.rec.x = nebData.frame * nebData.rec.width; // Update the X position of the rectangle based on the frame
+            nebData.frame++; // Increment the frame
+            if (nebData.frame > 7) // Check if the frame is greater than 7
             {
-                nebFrame = 0; // Reset the frame to 0
+                nebData.frame = 0; // Reset the frame to 0
             }
         }
 
-        scarfyPos.y += velocity * deltaTime; // Update scarfy position
-        nebPos.x += nebVelocity * deltaTime; // Update the nebula position
+        // Update nebula2 animation
+        nebData2.runningTime += deltaTime; // Update the running time
+        if (nebData2.runningTime>= nebData2.updateTime)
+        {
+            nebData2.runningTime = 0.0f; // Reset the running time
+            // updated animation frame
+            nebData2.rec.x = nebData2.frame * nebData2.rec.width; // Update the X position of the rectangle based on the frame
+            nebData2.frame; // Increment the frame
+            if (nebData2.frame > 7) // Check if the frame is greater than 7
+            {
+                nebData2.frame = 0; // Reset the frame to 0
+            }
+        }
 
-        DrawTextureRec(scarfy, scarfyRec, scarfyPos, RED); // Draw the scarfy character
-        DrawTextureRec(nebula, nebRec, nebPos, WHITE); // Draw the nebula hazard
+
+        scarfyData.pos.y += velocity * deltaTime; // Update scarfy position
+        nebData.pos.x += nebVelocity * deltaTime; // Update the nebula position
+        nebData2.pos.x += nebVelocity * deltaTime; // Update the second nebula position
+
+        DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, RED); // Draw the scarfy character
+        DrawTextureRec(nebula, nebData.rec, nebData2.pos, WHITE); // Draw the nebula hazard
+        DrawTextureRec(nebula, nebData2.rec, nebData2.pos, PURPLE); // Draw the second nebula hazard
 
         if (IsKeyPressed(KEY_SPACE) && !isAirborne) // Check if the space key is pressed
         {
             velocity = -jumpHeight; // Set the velocity when space is pressed
         }
-        else if (scarfyPos.y < windowHeight - scarfyRec.height) // Check if the rectangle is not on the ground
+        else if (scarfyData.pos.y < windowHeight - scarfyData.rec.height) // Check if the rectangle is not on the ground
         {
             velocity += gravity * deltaTime; // Increase the velocity to simulate gravity
             isAirborne = true; // Set the airborne state to true
@@ -105,7 +135,7 @@ int main()
         else // If the rectangle is on the ground
         {
             velocity = 0; // Set the velocity to 0
-            scarfyPos.y = windowHeight - scarfyRec.height; // Reset the Y position of the rectangle to the ground level
+            scarfyData.pos.y = windowHeight - scarfyData.rec.height; // Reset the Y position of the rectangle to the ground level
             isAirborne = false; // Set the airborne state to false
         }
 
